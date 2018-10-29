22263c9ef0dfd1ff149a9962171b769e8b8a375d
Taylor Cameron <taylorcameron95@hotmail.ca>
2018-10-29 20:53:14 +0000

    using a callback function to print

---
+++ add-callbacks/add-callbacks.js
@@ -0,0 +1,23 @@
+var https = require('https');
+var html = '';
+function printHTML(html) {
+  console.log(html);
+}
+var options = {
+  host: "sytantris.github.io",
+  path: "/http-examples/step4.html"
+};
+
+function getHTML(options, callback) {
+  https.get(options, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      html += data;
+    });
+    response.on('end', function() {
+      callback(html);
+    });
+  });
+}
+
+getHTML(options, printHTML);
